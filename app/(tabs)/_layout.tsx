caa8fe83ce0a3b54efa832b93c3ab18faabe4fd1
Abhishek Panwar <abhishekkpanwaarr@gmail.com>
2025-12-31 22:03:01 +0530

    refactor: remove unused components and clean up layout structure

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,31 +1,11 @@
-import FontAwesome from "@expo/vector-icons/FontAwesome";
-import { Tabs } from "expo-router";
-import React from "react";
-
-// import { useColorScheme } from '@/components/useColorScheme';
-// import { useClientOnlyValue } from '@/components/useClientOnlyValue';
 import { useTheme } from "@/context/ThemeContext";
 import { Ionicons } from "@expo/vector-icons";
-
-// You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
-function TabBarIcon(props: {
-  name: React.ComponentProps<typeof FontAwesome>["name"];
-  color: string;
-}) {
-  return <FontAwesome size={28} style={{ marginBottom: -3 }} {...props} />;
-}
+import { Tabs } from "expo-router";
 
 export default function TabLayout() {
-  // const colorScheme = useColorScheme();
   const { theme } = useTheme();
   return (
     <Tabs
-      // screenOptions={{
-      //   tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-      //   // Disable the static render of the header on web
-      //   // to prevent a hydration error in React Navigation v6.
-      //   headerShown: useClientOnlyValue(false, true),
-      // }}
       screenOptions={{
         tabBarStyle: {
           backgroundColor: theme.card,
@@ -81,34 +61,6 @@ export default function TabLayout() {
           ),
         }}
       />
-      {/* <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Tab One',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
-          headerRight: () => (
-            <Link href="/modal" asChild>
-              <Pressable>
-                {({ pressed }) => (
-                  <FontAwesome
-                    name="info-circle"
-                    size={25}
-                    color={Colors[colorScheme ?? 'light'].text}
-                    style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
-                  />
-                )}
-              </Pressable>
-            </Link>
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="two"
-        options={{
-          title: 'Tab Two',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
-        }}
-      /> */}
     </Tabs>
   );
 }
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,10 @@
 import { ThemeProvider } from "@/context/ThemeContext";
 import "@/global.css";
 import FontAwesome from "@expo/vector-icons/FontAwesome";
-// import {
-//   DarkTheme,
-//   DefaultTheme,
-//   ThemeProvider,
-// } from "@react-navigation/native";
 import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
+import { StatusBar } from "expo-status-bar";
 import { useEffect } from "react";
 import "react-native-reanimated";
 
@@ -17,12 +13,6 @@ export {
   ErrorBoundary,
 } from "expo-router";
 
-export const unstable_settings = {
-  // Ensure that reloading on `/modal` keeps a back button present.
-  initialRouteName: "(tabs)",
-};
-
-// Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
@@ -50,14 +40,12 @@ export default function RootLayout() {
 }
 
 function RootLayoutNav() {
-  // const colorScheme = useColorScheme();
-
   return (
     <ThemeProvider>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: "modal" }} />
       </Stack>
+      <StatusBar style="auto" />
     </ThemeProvider>
   );
 }
